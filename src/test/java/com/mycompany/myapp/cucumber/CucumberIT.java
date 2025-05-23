4ec160ccefde6c7b3faf79c1a2a3333f33a7502b
cutetoast <muhdmokhtar06@gmail.com>
2025-05-23 15:21:21 +0800

    Revert "Update dependencies in package.json and package-lock.json; upgrade @types/node, fork-ts-checker-webpack-plugin, puppeteer, typescript, and webpack versions. Add junit-platform-suite dependency in pom.xml. Refactor import statements in menu.tsx and routes.tsx for cleaner code. Update CucumberIT.java to use JUnit 5 suite annotations."
    This reverts commit 65192d7cf8be6bed18902b796d5542d1eb71a41a.

--- src/test/java/com/mycompany/myapp/cucumber/CucumberIT.java
+++ src/test/java/com/mycompany/myapp/cucumber/CucumberIT.java
@@ -1,17 +1,6 @@
 package com.mycompany.myapp.cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import com.mycompany.myapp.IntegrationTest;
 
-import org.junit.platform.suite.api.*;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameters(
-    {
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.mycompany.myapp.cucumber"),
-    }
-)
+@IntegrationTest
 class CucumberIT {}
--- src/test/resources/features/sample.feature
+++
@@ -1,9 +0,0 @@
-Feature: Sample Feature
-  As a user
-  I want to test the application
-  So that I can ensure it works correctly
-
-  Scenario: Sample scenario
-    Given the application is running
-    When I perform a test action
-    Then I should see the expected result 
