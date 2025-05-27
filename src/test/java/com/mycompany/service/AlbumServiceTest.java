7a02ba8dd15c14e5f835afec120b58116222a0e5
cutetoast <muhdmokhtar06@gmail.com>
2025-05-27 10:01:19 +0800

    test(AlbumServiceTest): fix import and formatting issues
    - Fix incorrect import path for AlbumService
    - Clean up unnecessary whitespace in test class

--- src/test/java/com/mycompany/service/AlbumServiceTest.java
+++ src/test/java/com/mycompany/service/AlbumServiceTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.service;
 
+import com.mycompany.myapp.service.AlbumService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
